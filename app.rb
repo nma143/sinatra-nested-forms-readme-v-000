6efeff57c5abcc8c9fd4fbb5bcb41373fda5eff7
Nikki A <nikki.apostolakis@gmail.com>
2018-10-05 17:08:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
 
   post '/student' do
     @student = Student.new(params[:student])
-    
+
      params[:student][:courses].each do |details|
        Course.new(details)
      end
     
      @courses = Course.all
-    
+
      erb :student
    end
